65ea41b5b8b20d4d6b362af581d29176cbafc348
uia66114 <nenad.samardzic@continental-corporation.com>
2020-07-11 00:33:52 +0200

    addec

---
+++ usefull_scripts/convert_to_html.ps1
@@ -0,0 +1,1 @@
+get-service | ConvertTo-Html -Title "Services" -Body "<H2>The result of get-service</H2> " -Property Name,DisplayName,Status | foreach {if($_ -like "*<td>Running</td>*"){$_ -replace "<tr>", "<tr bgcolor=green>"}elseif($_ -like "*<td>Stopped</td>*"){$_ -replace "<tr>", "<tr bgcolor=red>"}else{$_}} > d:\ss2.html
